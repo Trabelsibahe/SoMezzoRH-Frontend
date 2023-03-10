96395ecdf559a8d8888e2ce9ab6379251105a87d
Abir Ferjeni <abiirfr52@gmail.com>
2023-03-10 14:58:30 +0100

    front_login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 function LoginPage() {
   return (
-    <div>login</div>
+    <div className="App">
+    <img src="somezzologo BLANC.png"></img>
+    <div className="rect1">
+      <p className="ph1">ESPACE SOMEZZO RH</p>
+      <img src="print.png" className="print"></img>
+      <input type="text" id="matricule" name="matricule" placeholder="Matricule" required ></input>
+      <input type="text" id="mdp" name="mdp" placeholder="Mot De Passe" required ></input>
+      <input type="submit" value="connexion" id="conx"></input>
+      <p className='ph2'>Mot de passe oubilé? <a href=''> Récupérer votre compte</a></p>
+    </div>
+    <p className='ph3'>Tous droits réservés - SoMezzo</p>
+  </div>
   )
 }
 
