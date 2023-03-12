b9dd997af73f317cca5794756121b3694d87611c
Abir Ferjeni <abiirfr52@gmail.com>
2023-03-13 00:08:53 +0100

    page pas introuvable

--- src/pages/notfound.jsx
+++ src/pages/notfound.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import "../assets/styles/notfound.css"
+import textelogobleu from "../assets/images/textelogobleu.png";
+
 
 function NotFoundPage() {
   return (
-    <div>notfound</div>
+    <div className="NotFoundPage">
+    <img className="logoblue" src={textelogobleu}></img>
+  <div className="container">
+    <h2>Oops! Page non trouvée.</h2>
+    <h1>404 </h1>
+    <p>Nous ne pouvons pas trouver la page que vous recherchez. </p>
+    <a href='#'>Rentrer à votre espace</a>
+  </div>
+  </div>
   )
 }
 
