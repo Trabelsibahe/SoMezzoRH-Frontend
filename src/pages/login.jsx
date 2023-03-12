55394b15d03c3e37b7389ccb5c013986e1e3d38b
trabelsibahe <baha.trabelsi@gmail.com>
2023-03-12 15:27:42 +0100

    installed react-router-dom, bug fixes

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
+import "../assets/styles/login.css"
+import print from "../assets/images/print.png";
+import logoblanc from "../assets/images/logo_blanc.png"
 
 function LoginPage() {
+
   return (
-    <div className="App">
-    <img src="somezzologo BLANC.png"></img>
-    <div className="rect1">
+    <div className="LoginPage">
+      
+    <img className="logoblanc" src={logoblanc} alt="logo"></img>
+    <div className="login_container">
       <p className="ph1">ESPACE SOMEZZO RH</p>
-      <img src="print.png" className="print"></img>
-      <input type="text" id="matricule" name="matricule" placeholder="Matricule" required ></input>
-      <input type="text" id="mdp" name="mdp" placeholder="Mot De Passe" required ></input>
-      <input type="submit" value="connexion" id="conx"></input>
-      <p className='ph2'>Mot de passe oubilé? <a href=''> Récupérer votre compte</a></p>
+      <img src={print} alt="print" className="print"></img>
+
+
+      <div className='login_form'>
+      <input type="text" name="matricule" className='login_input' placeholder='Matricule'></input>
+      </div>
+      <div className='login_form'>
+      <input type="text" name="mdp"className='login_input' placeholder='Mot de passe'></input>
+      </div>
+
+      <p className='ph2'>Mot de passe oubilé?</p>
     </div>
     <p className='ph3'>Tous droits réservés - SoMezzo</p>
-  </div>
+    </div>
   )
 }
 
