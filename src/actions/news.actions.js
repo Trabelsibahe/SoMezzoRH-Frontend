6af7628a8d1457096310fd97167f558c480a1e7f
Abir Ferjeni <abiirfr52@gmail.com>
2023-04-05 01:15:21 +0200

    fonction ajouter et supprimer newslatter pour l expert

--- src/actions/news.actions.js
+++ src/actions/news.actions.js
@@ -18,3 +18,37 @@ export const listernews = () => {
             }
     }
 }
+export const addnews = (data) => {
+    return async dispatch => {
+        dispatch({type : newsConstants.ADD_NEWS_REQUEST})
+        try{
+            const res = await axios.post('http://127.0.0.1:3030/api/add/news', data)
+                    if (res.status === 200){
+                     dispatch({type : newsConstants.ADD_NEWS_SUCCESS,
+                        payload : { creatednews : res.data }
+                    })  
+                    }    
+        }catch(error){
+            dispatch({type : newsConstants.ADD_NEWS_FAILURE,
+                payload : { error : error.response}})  
+        }
+    }
+    }
+    export const Deletenews = (id)=>dispatch=>{
+        if(window.confirm("are you sure to delete this news?")){
+         axios
+         .delete(`http://127.0.0.1:3030/api/news/${id}`)
+         .then(res => {
+             dispatch({
+                 type: newsConstants.DELETE_NEWS,
+                 payload: id
+             })
+         })
+         .catch(err => {
+             dispatch({
+                 type: newsConstants.NEWS_ERRORS,
+                 payload: err.response.data
+             })
+         });
+        }
+     }
--- src/pages/newsletter.jsx
+++ src/pages/newsletter.jsx
@@ -1,37 +1,62 @@
 import Card from 'react-bootstrap/Card';
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {listernews,addnews} from '../actions/news.actions'
+import {listernews,addnews,Deletenews} from '../actions/news.actions'
 import Navigation from "../components/navigation";
 import "../assets/styles/news.css";
-
-
 import divider from "../components/divider"
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
 
 function NewsLetterPage() {
 
   const dispatch = useDispatch(); 
-    const news = useSelector(state => state.news.news)
-    const auth = useSelector((state) => state.auth);
-    useEffect(()=>{dispatch(listernews())},[])
- 
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
+  const news = useSelector(state => state.news.news)
+  const auth = useSelector((state) => state.auth);
+  useEffect(()=>{dispatch(listernews())},[])
 
-  const CurrentProfile = {
-    isConnected: auth.isConnected,
-    name: auth.user.utilisateur,
-    matricule: auth.user.matricule,
-    role: auth.user.role,
-    password : auth.user.password,
-  };
+const [ titre, setTitre ] = useState('');
+const [ description, setDescription ] = useState('');
+const [imgurl, setImgurl] = useState('');
 
+const [show, setShow] = useState(false);
+const handleClose = () => setShow(false);
+const handleShow = () => setShow(true);  
+const addnewsaction = async (e) => {
+  e.preventDefault();
+  const data = new FormData();
+  data.append('titre', titre);
+  data.append('description', description);
+  data.append('imgurl', imgurl);
+
+  await dispatch(addnews(data));
+  await dispatch(listernews());
+  handleClose();
+  setTitre('');
+  setDescription('');
+  setImgurl('');
+};
+const CurrentProfile = {
+  isConnected: auth.isConnected,
+  name: auth.user.utilisateur,
+  matricule: auth.user.matricule,
+  role: auth.user.role,
+  password : auth.user.password,
+};
+
+const deletenews = async (id) =>{
+  await dispatch(Deletenews(id))
+  await dispatch(listernews())
+  await dispatch(listernews())
+}
   return (
     <div className="news_page">
           <Navigation user={CurrentProfile} />
       <div className="news_container">
 
       <div className="page_name">
+      {CurrentProfile.role === "EXPERT" && (  <Button onClick={handleShow}>Ajouter</Button>)}
           Pages / Acceuil{" "}
           <p style={{ fontWeight: "bold", fontSize: "14px" }}>
             Newsletter
@@ -50,6 +75,7 @@ function NewsLetterPage() {
             <Card.Body>
               <Card.Title>{newsItem.titre}</Card.Title>
               <Card.Text>{newsItem.description}</Card.Text>
+              {CurrentProfile.role === "EXPERT" && ( <Button onClick={() => deletenews(newsItem._id)}>supprimer</Button>)}
             </Card.Body>
           </Card>
 
@@ -65,7 +91,38 @@ function NewsLetterPage() {
       )}
   
       </div>
+ {/** pop up add */}
+ <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Ajouter un news</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {" "}
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>titre</Form.Label>
+              <Form.Control type="text" value={titre} onChange={e => setTitre(e.target.value)} placeholder="Enter titre" />
+            </Form.Group>
 
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>description</Form.Label>
+              <Form.Control type="text" value={description} onChange={e => setDescription(e.target.value)} placeholder="numero" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+  <Form.Label>image</Form.Label>
+  <Form.Control type="file" name="imgurl" onChange={(e) => setImgurl(e.target.files[0])} />
+</Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={addnewsaction}>
+            Ajouter
+          </Button>
+        </Modal.Footer>
+      </Modal>
       </div>
         </div>    
         
