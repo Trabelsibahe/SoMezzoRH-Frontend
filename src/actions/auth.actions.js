42f68e35143db1d8e498aa95746016604672cc4c
trabelsibahe <baha.trabelsi@gmail.com>
2023-05-30 19:58:19 +0100

    change home page newsletter to slider/carousel, css updates

--- src/actions/auth.actions.js
+++ src/actions/auth.actions.js
@@ -5,139 +5,139 @@ import { setAuth } from '../util/setAuth';
 
 // register
 export const RegisterAction = (form, navigate) => dispatch => {
-    axios.post('http://127.0.0.1:3030/api/register', form)
-    .then(res=> {
-        navigate('/login');
-      
-        alert("création d'un nouvel utilisateur.")
-        dispatch({
-            type:authConstants.ERRORS,
-            payload: {}
-        })
-        })
-    .catch(err=> {
-        dispatch({
-            type: authConstants.ERRORS,
-            payload: err.response.data
-        })
+  axios.post('http://127.0.0.1:3030/api/register', form)
+    .then(res => {
+      navigate('/login');
+
+      alert("création d'un nouvel utilisateur.")
+      dispatch({
+        type: authConstants.ERRORS,
+        payload: {}
+      })
+    })
+    .catch(err => {
+      dispatch({
+        type: authConstants.ERRORS,
+        payload: err.response.data
+      })
     })
 }
 
 // login
-export const LoginAction = (form, navigate)=>dispatch=>{
-    axios.post('http://127.0.0.1:3030/api/login', form) 
+export const LoginAction = (form, navigate) => dispatch => {
+  axios.post('http://127.0.0.1:3030/api/login', form)
 
-    .then(res=>{
-        const {token} = res.data
-         localStorage.setItem('jwt', token);
-         const decode = jwt_decode(token);
-         dispatch(setUser(decode));
-         setAuth(token);
-         alert("Bievenue " +decode.matricule);
-         window.location.reload();
-         navigate("/bienvenue")
+    .then(res => {
+      const { token } = res.data
+      localStorage.setItem('jwt', token);
+      const decode = jwt_decode(token);
+      dispatch(setUser(decode));
+      setAuth(token);
+      alert("Bievenue " + decode.matricule);
+      window.location.reload();
+      navigate("/bienvenue")
     })
-    .catch(err=>{
-        dispatch({
-            type: authConstants.ERRORS,
-            payload: err.response.data
-        })
+    .catch(err => {
+      dispatch({
+        type: authConstants.ERRORS,
+        payload: err.response.data
+      })
     })
 
 }
 
 // Change Password 
 export const ChangePasswordAction = (form, navigate) => dispatch => {
-    axios.post('http://127.0.0.1:3030/api/modifmotpass', form)
+  axios.post('http://127.0.0.1:3030/api/modifmotpass', form)
 
     .then(res => {
-        alert("Mot de passe modifié, veuillez reconnecter")
-        localStorage.clear()
-        window.location.reload()
-        dispatch({
-            type: authConstants.SET_USER,
-            payload: res.data
-        })
+      alert("Mot de passe modifié, veuillez reconnecter")
+      localStorage.clear()
+      window.location.reload()
+      dispatch({
+        type: authConstants.SET_USER,
+        payload: res.data
+      })
 
     })
-    .catch (err => {
-        dispatch({
-            type: authConstants.ERRORS,
-            payload: err.response.data
-        })
+    .catch(err => {
+      dispatch({
+        type: authConstants.ERRORS,
+        payload: err.response.data
+      })
     })
 }
 
 // logout
-export const Logout = (navigate)=>dispatch=>{
-    localStorage.removeItem('jwt')
-    navigate("/login")
+export const Logout = (navigate) => dispatch => {
+  localStorage.removeItem('jwt')
+  navigate("/login")
 
-    dispatch({
-        type: authConstants.SET_USER,
-        payload: {}
-    })
+  dispatch({
+    type: authConstants.SET_USER,
+    payload: {}
+  })
 }
 
 
-export const setUser = (decode)=>({
-    type: authConstants.SET_USER,
-    payload: decode
+export const setUser = (decode) => ({
+  type: authConstants.SET_USER,
+  payload: decode
 })
 
-  export const sendPasswordResetEmail = (email) => async (dispatch) => {
+export const sendPasswordResetEmail = (email) => async (dispatch) => {
+  try {
+    await axios.post('http://127.0.0.1:3030/api/mot-de-passe-oublie', { email });
+
+    dispatch({
+      type: authConstants.SEND_EMAIL_SUCCESS,
+      payload: "Un e-mail de réinitialisation de mot de passe a été envoyé.",
+    });
+  } catch (error) {
+    dispatch({
+      type: authConstants.SEND_EMAIL_FAILURE,
+      payload: "Une erreur s'est produite lors de l'envoi de l'e-mail de réinitialisation.",
+    });
+  }
+};
+
+export const resetPassword = (resetToken, newPassword, email) => async (dispatch) => {
+  try {
+    const res = await axios.post('http://127.0.0.1:3030/api/new-mot-de-passe', { resetToken, newPassword, email });
+
+    alert("Mot de passe modifié, veuillez vous reconnecter");
+    localStorage.clear();
+    window.location.reload();
+    dispatch({
+      type: authConstants.RESET_PASSWORD_SUCCESS,
+      payload: "Votre mot de passe a été réinitialisé avec succès.",
+    });
+  } catch (error) {
+    dispatch({
+      type: authConstants.RESET_PASSWORD_FAILURE,
+      payload: "Une erreur s'est produite lors de la réinitialisation du mot de passe.",
+    });
+  }
+};
+
+export const Getuser = () => {
+  return async dispatch => {
+    dispatch({ type: authConstants.USER_REQUEST })
     try {
-      await axios.post('http://127.0.0.1:3030/api/mot-de-passe-oublie', { email });
-  
-      dispatch({
-        type: authConstants.SEND_EMAIL_SUCCESS,
-        payload: "Un e-mail de réinitialisation de mot de passe a été envoyé.",
-      });
-    } catch (error) {
-      dispatch({
-        type: authConstants.SEND_EMAIL_FAILURE,
-        payload: "Une erreur s'est produite lors de l'envoi de l'e-mail de réinitialisation.",
-      });
-    }
-  };
-  
-  export const resetPassword = (resetToken, newPassword, email) => async (dispatch) => {
-    try {
-      const res = await axios.post('http://127.0.0.1:3030/api/new-mot-de-passe', { resetToken, newPassword, email });
-  
-      alert("Mot de passe modifié, veuillez vous reconnecter");
-      localStorage.clear();
-      window.location.reload();
-      dispatch({
-        type: authConstants.RESET_PASSWORD_SUCCESS,
-        payload: "Votre mot de passe a été réinitialisé avec succès.",
-      });
-    } catch (error) {
-      dispatch({
-        type: authConstants.RESET_PASSWORD_FAILURE,
-        payload: "Une erreur s'est produite lors de la réinitialisation du mot de passe.",
-      });
-    }
-  };
-  
-  export const Getuser = () => {
-    return async dispatch => {
-      dispatch({ type: authConstants.USER_REQUEST })
-      try {
-        const res = await axios.get('http://127.0.0.1:3030/api/alluser')
-        if (res.status === 200) {
-          dispatch({
-            type: authConstants.GET_ALL_USER,
-            payload: res.data
-          })
-        }
-      } catch (error) {
+      const res = await axios.get('http://127.0.0.1:3030/api/alluser')
+      if (res.status === 200) {
         dispatch({
-          type: authConstants.USER_ERRORS,
-          payload: { error: error.response }
+          type: authConstants.GET_ALL_USER,
+          payload: res.data
         })
-  
       }
+    } catch (error) {
+      dispatch({
+        type: authConstants.USER_ERRORS,
+        payload: { error: error.response }
+      })
+
     }
   }
-  
+}
+
--- src/pages/newsletter.jsx
+++ src/pages/newsletter.jsx
@@ -1,26 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import CircularProgress from "@mui/material/CircularProgress";
 import Card from "react-bootstrap/Card";
 import Button from "@mui/material/Button";
 import Form from "react-bootstrap/Form";
 import { Navigate } from "react-router-dom";
 import Classnames from "classnames";
-import { Box, Modal } from "@mui/material";
-import image from "../assets/images/banner.avif"
-import {
-  listernews,
-  addnews,
-  Deletenews,
-  supprimerNews,
-} from "../actions/news.actions";
-import {
-  SendNotificationToAll,
-  SendNotificationToOneUser,
-} from "../actions/notification.action";
+import { Box, Modal, Skeleton } from "@mui/material";
 import Navigation from "../components/navigation";
 import "../assets/styles/news.css";
-import divider from "../components/divider";
+import test from "../assets/images/banner.avif";
+import somezzologo from "../assets/images/icone.png";
+import logoblanc from "../assets/images/logo_blanc.png";
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
+import { listernews, addnews, Deletenews, supprimerNews, } from "../actions/news.actions";
+import { SendNotificationToAll, SendNotificationToOneUser, } from "../actions/notification.action";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import the carousel styles
+import { Carousel } from "react-responsive-carousel"; // Import the Carousel component
 
 const style = {
   position: "absolute",
@@ -38,71 +34,9 @@ const style = {
 };
 
 function NewsLetterPage() {
-
-  useEffect(() => {
-    const el = document.querySelector('.slider');
-    const slides = Array.from(el.querySelectorAll('li'));
-    const nav = Array.from(el.querySelectorAll('nav a'));
-    const totalSlides = slides.length;
-    let current = 0;
-    let autoPlay = true;
-    const timeTrans = 4000;
-    const indexElements = [];
-
-    for (let i = 0; i < totalSlides; i++) {
-      indexElements.push(i);
-    }
-
-    const setCurret = () => {
-      slides[current].classList.add('current');
-      nav[current].classList.add('current_dot');
-    };
-
-    const changeSlide = (index) => {
-      nav.forEach((allDot) => allDot.classList.remove('current_dot'));
-
-      slides.forEach((allSlides) =>
-        allSlides.classList.remove('prev', 'current')
-      );
-
-      const getAllPrev = (value) => value < index;
-
-      const prevElements = indexElements.filter(getAllPrev);
-
-      prevElements.forEach((indexPrevEle) =>
-        slides[indexPrevEle].classList.add('prev')
-      );
-
-      slides[index].classList.add('current');
-      nav[index].classList.add('current_dot');
-    };
-
-    const initEvents = () => {
-      nav.forEach((dot) => {
-        dot.addEventListener('click', (ele) => {
-          ele.preventDefault();
-          changeSlide(nav.indexOf(dot));
-        });
-      });
-
-      el.addEventListener('mouseenter', () => (autoPlay = false));
-      el.addEventListener('mouseleave', () => (autoPlay = true));
-
-      setInterval(() => {
-        if (autoPlay) {
-          current = current < slides.length - 1 ? current + 1 : 0;
-          changeSlide(current);
-        }
-      }, timeTrans);
-    };
-
-    setCurret();
-    initEvents();
-  }, []);
-
   const dispatch = useDispatch();
   const news = useSelector((state) => state.news.news);
-  const [loading, setLoading] = useState(false); // New loading state
+  const [loading, setLoading] = useState(false);
   const auth = useSelector((state) => state.auth);
   const errors = useSelector((state) => state.errors);
 
@@ -110,7 +44,7 @@ function NewsLetterPage() {
     message: "Une nouvelle news a été ajoutée.",
   };
 
-  
+  // ...
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
@@ -120,30 +54,27 @@ function NewsLetterPage() {
 
     fetchData();
   }, []);
-
   // ...
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      await dispatch(supprimerNews());
-      setLoading(false);
-    };
-
-    fetchData();
-  }, []);
-  const [currentSlide, setCurrentSlide] = useState(0);
 
   const [titre, setTitre] = useState("");
   const [description, setDescription] = useState("");
   const [dateSuppression, setDateSuppression] = useState("");
   const [imgurl, setImgurl] = useState("");
-  const [currentIndex, setCurrentIndex] = useState(0);
 
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   const [error, setError] = useState("");
+
+  const CurrentProfile = {
+    isConnected: auth.isConnected,
+    nom: auth.user.nom,
+    prenom: auth.user.prenom,
+    matricule: auth.user.matricule,
+    role: auth.user.role,
+    password: auth.user.password,
+    active: auth.user.active,
+  };
 
   const addnewsaction = async (e) => {
     e.preventDefault();
@@ -170,178 +101,165 @@ function NewsLetterPage() {
       setDescription("");
       setTitre("");
     }
-    setLoading(false); // Set loading to false after fetching data
+    setLoading(false);
   };
 
   const deletenewsaction = async (id) => {
     await dispatch(Deletenews(id));
     await dispatch(listernews());
-    await dispatch(listernews());
   };
 
-  const CurrentProfile = {
-    isConnected: auth.isConnected,
-    nom: auth.user.nom,
-    prenom: auth.user.prenom,
-    matricule: auth.user.matricule,
-    role: auth.user.role,
-    password: auth.user.password,
-    active: auth.user.active,
-  };
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      await dispatch(supprimerNews());
+      setLoading(false);
+    };
 
-  if (CurrentProfile.active === false) {
-    return <Navigate to="/inactive" />;
-  }
+    fetchData();
+  }, []);
 
   return (
     <div className="news_page">
       <Navigation user={CurrentProfile} />
       <div className="news_container">
-      <section className="intro">
-      <div className="left">
-        <div>
-          <span>Explore</span>
-          <h1>The Places Where You Will</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos
-            eveniet amet excepturi voluptates dolorem totam ad quod hic, porro
-            accusamus, repellat, corrupti at obcaecati ducimus, dolor quibusdam
-            sequi nemo inventore?
-          </p>
-          <a href="https://unsplash.com/" target="_blank">
-            Images by Unsplash
-          </a>
+        <div className="news_body">
+
+          {news && news.length > 0 ? (
+            <Carousel showArrows={true} showThumbs={false}>
+              {news.map((newsItem, index) => (
+                <div className="news_content" key={index}>
+                 <span className="news_header">
+                  <img style={{ width: "100px" }} src={logoblanc} alt=""></img>
+                  <Button variant="outlined" color="secondary" size="small"  startIcon={<AddBoxOutlinedIcon />} sx={{ color: "white" }}
+                   onClick={handleShow} >Ajouter</Button> </span>
+                  <div className="news_text">
+                    <h1>{newsItem.titre}</h1>
+                    <p>{newsItem.description}</p>
+                    <Button
+                      startIcon={<DeleteOutlineIcon />}
+                      sx={{ color: "white" }}
+                      onClick={() => deletenewsaction(newsItem._id)}
+                    >
+                      Supprimer
+                    </Button>
+                  </div>
+
+                  <div className="news_image">
+                    {newsItem.imgurl ? <Card.Img
+                      style={{
+                        padding: "1em",
+                        borderTopLeftRadius: "50%",
+                        borderBottomLeftRadius: "20%",
+                        borderBottomRightRadius: "45%",
+                        borderTopRightRadius: "30%",
+                        border: "4px solid #ebf0f7",
+                        maxHeight: "450px"
+                      }}
+                      variant="top"
+                      src={`http://localhost:3030/${newsItem?.imgurl}`}
+                    /> : <span style={{maxWidth:"450px", background:"gray"}}>
+                      </span>}
+                  </div>
+                </div>
+              ))}
+            </Carousel>
+          ) : (
+            <div className="news_content">
+                    <span className="news_header">
+                  <img style={{ width: "100px" }} src={logoblanc ? logoblanc : logoblanc} alt=""></img>
+                  <Button variant="outlined" color="secondary" size="small"  startIcon={<AddBoxOutlinedIcon />} sx={{ color: "white" }}
+                   onClick={handleShow} >Ajouter</Button> </span>
+            </div>
+          )}
+        </div>
+
+        <div className="newsletter_footer">
+          <p>Tous droits réservés - SoMezzo</p>
+          <img src={somezzologo} alt="logo"></img>
         </div>
       </div>
-
-      <div className="slider">
-        <ul>
-          <li
-            style={{
-              backgroundImage:
-                'url(https://images.unsplash.com/photo-1458640904116-093b74971de9?crop=entropy&fit=crop&fm=jpg&h=675&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1375)',
-            }}
-          >
-            <div className="center-y">
-              <h3>Slider Title #1</h3>
-              <a href="#">View Project</a>
-            </div>
-          </li>
-          <li
-            style={{
-              backgroundImage:
-                'url(https://images.unsplash.com/photo-1451906278231-17b8ff0a8880?crop=entropy&fit=crop&fm=jpg&h=675&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1375)',
-            }}
-          >
-            <div className="center-y">
-              <h3>Slider Title #2</h3>
-              <a href="#">View Project</a>
-            </div>
-          </li>
-          <li
-            style={{
-              backgroundImage:
-                'url(https://images.unsplash.com/photo-1456428199391-a3b1cb5e93ab?crop=entropy&fit=crop&fm=jpg&h=675&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1375)',
-            }}
-          >
-            <div className="center-y">
-              <h3>Slider Title #3</h3>
-              <a href="#">View Project</a>
-            </div>
-          </li>
-        </ul>
-
-        <ul>
-          <nav>
-            <a href="#"></a>
-            <a href="#"></a>
-            <a href="#"></a>
-          </nav>
-        </ul>
-      </div>
-    </section>
-        {/** pop up modal  add */}
-        <Modal open={show} onHide={handleClose}>
-          <form className="news_form">
-            <Box sx={style}>
-              <p className="task_add_name">Ajouter une newsletter</p>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Titre</Form.Label>
-                <Form.Control
-                  type="text"
-                  value={titre}
-                  onChange={(e) => setTitre(e.target.value)}
-                  placeholder="Titre"
-                  className={Classnames("w-100", {
-                    "is-invalid": errors.titre,
-                  })}
-                />
-                {errors.titre && (
-                  <div className="invalid-feedback">{errors.titre}</div>
-                )}
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Déscription</Form.Label>
-                <Form.Control
-                  type="text"
-                  value={description}
-                  onChange={(e) => setDescription(e.target.value)}
-                  placeholder="Déscription"
-                  className={Classnames("w-100", {
-                    "is-invalid": errors.description,
-                  })}
-                />
-                {errors.description && (
-                  <div className="invalid-feedback">{errors.description}</div>
-                )}
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Date d'expiration</Form.Label>
-                <Form.Control
-                  type="date"
-                  value={dateSuppression}
-                  onChange={(e) => setDateSuppression(e.target.value)}
-                  placeholder="date Suppression"
-                  className={Classnames("w-100", {
-                    "is-invalid": errors.dateSuppression,
-                  })}
-                  disablePast
-                  min={new Date().toISOString().split("T")[0]}
-                />
-                {errors.dateSuppression && (
-                  <div className="invalid-feedback">
-                    {errors.dateSuppression}
-                  </div>
-                )}
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>
-                  Charger une image pour votre newsletter.
-                </Form.Label>
-                <Form.Control
-                  type="file"
-                  name="imgurl"
-                  onChange={(e) => setImgurl(e.target.files[0])}
-                  className={Classnames("w-100", {
-                    "is-invalid": errors.imgurl,
-                  })}
-                />
-                {errors.imgurl && (
-                  <div className="invalid-feedback">{errors.imgurl}</div>
-                )}
-              </Form.Group>
-              <p style={{ color: "red", textAlign: "center" }}>{error}</p>
-              <Button variant="outlined" onClick={handleClose}>
-                Annuler
-              </Button>{" "}
-              <Button variant="outlined" onClick={addnewsaction}>
-                Publier
-              </Button>
-            </Box>
-          </form>
-        </Modal>
-      </div>
-      </div>
+      {/** pop up modal  add */}
+      <Modal open={show} onClose={handleClose}>
+        <form className="news_form">
+          <Box sx={style}>
+            <p className="task_add_name">Ajouter une newsletter</p>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Titre</Form.Label>
+              <Form.Control
+                type="text"
+                value={titre}
+                onChange={(e) => setTitre(e.target.value)}
+                placeholder="Titre"
+                className={Classnames("w-100", {
+                  "is-invalid": errors.titre,
+                })}
+              />
+              {errors.titre && (
+                <div className="invalid-feedback">{errors.titre}</div>
+              )}
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Déscription</Form.Label>
+              <Form.Control
+                type="text"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                placeholder="Déscription"
+                className={Classnames("w-100", {
+                  "is-invalid": errors.description,
+                })}
+              />
+              {errors.description && (
+                <div className="invalid-feedback">{errors.description}</div>
+              )}
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Date d'expiration</Form.Label>
+              <Form.Control
+                type="date"
+                value={dateSuppression}
+                onChange={(e) => setDateSuppression(e.target.value)}
+                placeholder="date Suppression"
+                className={Classnames("w-100", {
+                  "is-invalid": errors.dateSuppression,
+                })}
+                disablePast
+                min={new Date().toISOString().split("T")[0]}
+              />
+              {errors.dateSuppression && (
+                <div className="invalid-feedback">
+                  {errors.dateSuppression}
+                </div>
+              )}
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>
+                Charger une image pour votre newsletter.
+              </Form.Label>
+              <Form.Control
+                type="file"
+                name="imgurl"
+                onChange={(e) => setImgurl(e.target.files[0])}
+                className={Classnames("w-100", {
+                  "is-invalid": errors.imgurl,
+                })}
+              />
+              {errors.imgurl && (
+                <div className="invalid-feedback">{errors.imgurl}</div>
+              )}
+            </Form.Group>
+            <p style={{ color: "red", textAlign: "center" }}>{error}</p>
+            <Button variant="outlined" onClick={handleClose}>
+              Annuler
+            </Button>{" "}
+            <Button variant="outlined" onClick={addnewsaction}>
+              Publier
+            </Button>
+          </Box>
+        </form>
+      </Modal>
+    </div>
   );
 }
 
